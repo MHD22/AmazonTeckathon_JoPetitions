04e3e3dfe2f386bbbac293dbabe1903cd45335d8
Hasan Qattam <hassan.qattam@hotmail.com>
2020-09-16 07:58:59 +0300

    UI Changes

--- jo_petitions/src/Components/HomePage/Navbar.js
+++ jo_petitions/src/Components/HomePage/Navbar.js
@@ -1,23 +1,63 @@
-import React from 'react';
-import { Navbar , Nav, Form,FormControl, Button } from 'react-bootstrap';
-
-function NavBar({changeRoute}){
-return(
+import React from "react";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import "./Footer.css";
+function NavBar({ changeRoute }) {
+  return (
     <Navbar bg="light" variant="light shadow-5 ">
-    <Navbar.Brand onClick={()=>{changeRoute('home')}} className="nav-head pointer dim">JO Petitions</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link onClick={()=>{changeRoute('start')}}> Start a petition</Nav.Link>
-      <Nav.Link onClick={()=>{changeRoute('my petitions')}}>My petitions</Nav.Link>
-      <Nav.Link onClick={()=>{changeRoute('browse')}}>Browse</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button onClick={()=>{changeRoute('sign in')}} variant="outline-primary dim mh2">Login</Button>
-      <Button onClick={()=>{changeRoute('register')}} variant="outline-primary dim">Rigester</Button>
-    </Form>
-  </Navbar>
-);
-
+      <Navbar.Brand
+        onClick={() => {
+          changeRoute("home");
+        }}
+        className="nav-head pointer dim"
+        className="logo-color"
+      >
+        JoPetitions
+      </Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link
+          onClick={() => {
+            changeRoute("start");
+          }}
+        >
+          {" "}
+          Start a petition
+        </Nav.Link>
+        <Nav.Link
+          onClick={() => {
+            changeRoute("my petitions");
+          }}
+        >
+          My petitions
+        </Nav.Link>
+        <Nav.Link
+          onClick={() => {
+            changeRoute("browse");
+          }}
+        >
+          Browse
+        </Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button
+          onClick={() => {
+            changeRoute("sign in");
+          }}
+          variant="outline-success dim mh2 "
+        >
+          Login
+        </Button>
+        <Button
+          onClick={() => {
+            changeRoute("register");
+          }}
+          variant="outline-success dim"
+        >
+          Register
+        </Button>
+      </Form>
+    </Navbar>
+  );
 }
 
-export default NavBar ;
+export default NavBar;
